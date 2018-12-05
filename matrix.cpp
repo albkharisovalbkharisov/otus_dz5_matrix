497d2b980351017c085cffd55501e3caf5d9f2ca
Albert Kharisov <ah@bright-box.com>
2018-12-06 02:38:03 +0300

    step step

--- matrix.cpp
+++ matrix.cpp
@@ -51,16 +51,23 @@ class Matrix
     data_container m;
 
 public:
+    class MatrixIt;
     auto operator[](const size_t &col)
     {
         miniMatrix o{this, col};
         return o;
     }
 
-    miniMatrix begin(void)
+    MatrixIt begin(void)
     {
-        ;
-            if (search != mat->m.end()) {
+        std::cout << "begin()" << std::endl;
+        return MatrixIt{m.begin()};
+    }
+
+    MatrixIt end(void)
+    {
+        std::cout << "end()" << std::endl;
+        return MatrixIt{m.end()};
     }
 
     size_t size(void)
@@ -68,7 +75,49 @@ public:
         return m.size();
     }
 
-    ///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+    struct MatrixIt
+    {
+        typename data_container::iterator it;
+        MatrixIt(decltype(it) &&it) : it{it} {}
+
+        MatrixIt& operator++()
+        {
+            std::cout << "It op++" << std::endl;
+            ++it;
+            return *this;
+        }
+
+        MatrixIt& operator*()
+        {
+            std::cout << "It op*" << std::endl;
+//            size_t col, row;
+//            std::cout << "before col/row = " << col << "/" << row << std::endl;
+//            std::tie<col, row> = it->first;
+//            std::cout << "after col/row = " << col << "/" << row << std::endl;
+            return *this;
+        }
+
+        bool operator!=(const MatrixIt& other)
+        {
+            std::cout << "It op!=" << std::endl;
+            return this->it != other.it;
+        }
+
+        template <typename S1, typename S2, typename T1>
+        operator std::tuple<S1, S2, T1>()
+        {
+            std::cout << "It castTuple" << std::endl;
+            // remove 'const' for passing to tuple only. It's safe.
+//            return std::tuple<S1, S2, T1>(row, col, const_cast<T&>(gett()));
+            S1 row = it->first.row;
+            S2 col = it->first.col;
+            return std::tuple<S1, S2, T1>(row, col, it->second);
+//            return std::tuple<S1, S2, T1>(it->first.row, it->first.col, it->second);
+        }
+    };
+
+///////////////////////////////////////////////////////////
     class miniMatrix
     {
     private:
@@ -87,12 +136,8 @@ public:
         size_t col;
         size_t row;
     public:
-        miniMatrix(class Matrix *mat, size_t col) : mat(mat), col(col), it(mat->m.begin()) {}
-        miniMatrix(decltype(mat->m.begin()) it) {
-            it(mat->m.begin());
-        }
-        decltype(mat->m.begin()) it;
-        
+        miniMatrix(class Matrix *mat, size_t col) : mat(mat), col(col) {}
+
         bool operator==(const T& other)
         {
             return this->gett() == other;
@@ -121,44 +166,36 @@ public:
             return *this;
         }
 
-        auto begin(void)
-        {
-            return miniMatrix{mat->m.begin()};
-        }
-
-        auto end(void)
-        {
-            return miniMatrix{mat->m.end()};
-        }
-
         friend std::ostream& operator<< (std::ostream &os, miniMatrix &mm )
         {
             std::cout << "[" << mm.col << "][" << mm.row << "] = " << mm.gett();
             return os;
         }
 
-        template <typename S1, typename S2, typename T1>
-        operator std::tuple<S1, S2, T1>()
-        {
-            // remove 'const' for passing to tuple only. It's safe.
-            return std::tuple<S1, S2, T1>(row, col, const_cast<T&>(gett()));
-        }
+//        template <typename S1, typename S2, typename T1>
+//        operator std::tuple<S1, S2, T1>()
+//        {
+//            // remove 'const' for passing to tuple only. It's safe.
+//            return std::tuple<S1, S2, T1>(row, col, const_cast<T&>(gett()));
+//        }
     };
 };
+
+
 
 int main()
 {
     Matrix<int, -1> matrix; // бесконечная матрица int заполнена значениями -1
     matrix[100][100] = 314;
 
-//    for(auto c: matrix)
-//    {
-//        int x;
-//        int y;
-//        int v;
-//        std::tie(x, y, v) = c;
-//        std::cout << x << y << v << std::endl;
-//    }
+    for(auto c: matrix)
+    {
+        size_t x = 1;
+        size_t y = 2;
+        int v = 3;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
 //    return 0;
 }
 
